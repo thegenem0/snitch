8af37e21719d12e095bbc5d736264ae1039f5a45
thegenem0 <nemethgergo02@gmail.com>
2025-05-17 21:18:03 +0100

    Add FIXME(#6)

--- project.go
+++ project.go
@@ -9,6 +9,7 @@ import (
 	"os/exec"
 	"path"
 	"regexp"
+	"strings"
 
 	"github.com/pkg/errors"
 	"gopkg.in/yaml.v2"
@@ -16,8 +17,8 @@ import (
 
 // TransformRule defines a title transformation rule
 type TransformRule struct {
-	Match   string
-	Replace string
+	Match   string `yaml:"match,omitempty"`
+	Replace string `yaml:"replace,omitempty"`
 }
 
 // Transform applies a title transformation rule
@@ -29,7 +30,7 @@ func (transformRule *TransformRule) Transform(title string) string {
 
 // TitleConfig contains project level configuration related to issue titles
 type TitleConfig struct {
-	Transforms []*TransformRule
+	Transforms []*TransformRule `yaml:"transforms,omitempty"`
 }
 
 // Transform transforms the suffix into the title
@@ -43,88 +44,222 @@ func (titleConfig *TitleConfig) Transform(title string) string {
 
 const defaultBodySeparator = "---"
 
+var gitUserNameCache string
+
 // Project contains the project level configuration
 type Project struct {
-	Title         *TitleConfig
-	Keywords      []string
-	BodySeparator string
-	Remote        string
+	Title         *TitleConfig `yaml:"title,omitempty"`
+	Keywords      []string     `yaml:"keywords,omitempty"`
+	BodySeparator string       `yaml:"body_separator,omitempty"`
+	Remote        string       `yaml:"remote,omitempty"`
+	UseUsernames  bool         `yaml:"use_usernames,omitempty"`
 }
 
-func unreportedTodoRegexp(keyword string) string {
-	return "^(.*)" + regexp.QuoteMeta(keyword) + "(" + regexp.QuoteMeta(string(keyword[len(keyword)-1])) + "*)" + ": (.*)$"
+func simpleUnreportedTodoRegexp(keyword string) string {
+	return "^(.*)" +
+		regexp.QuoteMeta(keyword) +
+		"(" + regexp.QuoteMeta(string(keyword[len(keyword)-1])) + "*)" +
+		": (.*)$"
 }
 
-func reportedTodoRegexp(keyword string) string {
+func unamePrefixUnreportedTodoRegexp(keyword string) string {
+	return "^(.*?)" +
+		regexp.QuoteMeta(keyword) +
+		"(" + regexp.QuoteMeta(string(keyword[len(keyword)-1])) + "*)" +
+		"\\(([^)]+)\\)" + // Username
+		":[ \t]*(.*)$" // Suffix
+}
+
+func simpleReportedTodoRegexp(keyword string) string {
 	return "^(.*)" + regexp.QuoteMeta(keyword) + "(" + regexp.QuoteMeta(string(keyword[len(keyword)-1])) + "*)" + "\\((.*)\\): (.*)$"
 }
 
-func (project Project) lineAsUnreportedTodo(line string) *Todo {
-	for _, keyword := range project.Keywords {
-		unreportedTodo := regexp.MustCompile(
-			unreportedTodoRegexp(keyword))
-		groups := unreportedTodo.FindStringSubmatch(line)
-
-		if groups != nil {
-			prefix := groups[1]
-			urgency := groups[2]
-			suffix := groups[3]
-			title := project.Title.Transform(suffix)
-
-			return &Todo{
-				Prefix:        prefix,
-				Suffix:        suffix,
-				Keyword:       keyword,
-				Urgency:       len(urgency),
-				ID:            nil,
-				Filename:      "",
-				Line:          0,
-				Title:         title,
-				BodySeparator: project.BodySeparator,
-			}
-		}
-	}
-
-	return nil
+func unamePrefixReportedTodoRegexp(keyword string) string {
+	return "^(.*?)" +
+		regexp.QuoteMeta(keyword) +
+		"(" + regexp.QuoteMeta(string(keyword[len(keyword)-1])) + "*)" +
+		"\\(([^)]+)\\)" + // Username
+		"\\[(#\\d+)\\]" + // Issue ID
+		":[ \t]*(.*)$" // Suffix
 }
 
-func (project Project) lineAsReportedTodo(line string) *Todo {
-	for _, keyword := range project.Keywords {
-		unreportedTodo := regexp.MustCompile(reportedTodoRegexp(keyword))
-		groups := unreportedTodo.FindStringSubmatch(line)
-
-		if groups != nil {
-			prefix := groups[1]
-			urgency := groups[2]
-			id := groups[3]
-			suffix := groups[4]
-			title := project.Title.Transform(suffix)
-
-			return &Todo{
-				Prefix:        prefix,
-				Suffix:        suffix,
-				Keyword:       keyword,
-				Urgency:       len(urgency),
-				ID:            &id,
-				Filename:      "",
-				Line:          0,
-				Title:         title,
-				BodySeparator: project.BodySeparator,
-			}
-		}
+func getGitUserName() string {
+	if gitUserNameCache != "" {
+		return gitUserNameCache
 	}
 
-	return nil
+	cmd := exec.Command("git", "config", "--get", "user.name")
+	output, err := cmd.Output()
+
+	if err != nil {
+		return "unknown"
+	} else {
+		username := strings.TrimSpace(string(output))
+		if username == "" {
+			gitUserNameCache = "unknown"
+		} else {
+			gitUserNameCache = username
+		}
+	}
+	return gitUserNameCache
+}
+
+func (project Project) lineAsSimpleUnreportedTodo(line string, keyword string) *Todo {
+	unreportedTodo := regexp.MustCompile(
+		simpleUnreportedTodoRegexp(keyword))
+	groups := unreportedTodo.FindStringSubmatch(line)
+
+	if groups == nil {
+		return nil
+	}
+
+	prefix := groups[1]
+	urgency := groups[2]
+	suffix := groups[3]
+	title := project.Title.Transform(suffix)
+
+	return &Todo{
+		Prefix:        prefix,
+		Suffix:        suffix,
+		Keyword:       keyword,
+		Urgency:       len(urgency),
+		ID:            nil,
+		Filename:      "",
+		Line:          0,
+		Title:         title,
+		BodySeparator: project.BodySeparator,
+	}
+}
+
+func (project Project) lineAsSimpleReportedTodo(line string, keyword string) *Todo {
+	reportedTodo := regexp.MustCompile(simpleReportedTodoRegexp(keyword))
+	groups := reportedTodo.FindStringSubmatch(line)
+
+	if groups == nil {
+		return nil
+	}
+
+	prefix := groups[1]
+	urgency := groups[2]
+	id := groups[3]
+	suffix := groups[4]
+	title := project.Title.Transform(suffix)
+
+	return &Todo{
+		Prefix:        prefix,
+		Suffix:        suffix,
+		Keyword:       keyword,
+		Urgency:       len(urgency),
+		ID:            &id,
+		Filename:      "",
+		Line:          0,
+		Title:         title,
+		BodySeparator: project.BodySeparator,
+	}
+}
+
+func (project Project) lineAsUsernamePrefixUnreportedTodo(line string, keyword string) *Todo {
+	reportedTodo := regexp.MustCompile(unamePrefixUnreportedTodoRegexp(keyword))
+	groups := reportedTodo.FindStringSubmatch(line)
+
+	if groups == nil {
+		return nil
+	}
+
+	prefix := groups[1]
+	urgency := groups[2]
+	username := groups[3]
+	suffix := groups[4]
+	title := project.Title.Transform(suffix)
+
+	return &Todo{
+		Prefix:        prefix,
+		Suffix:        suffix,
+		Keyword:       keyword,
+		Urgency:       len(urgency),
+		Username:      username,
+		ID:            nil,
+		Filename:      "",
+		Line:          0,
+		Title:         title,
+		BodySeparator: project.BodySeparator,
+	}
+}
+
+func (project Project) lineAsUsernamePrefixReportedTodo(line string, keyword string) *Todo {
+	reportedTodo := regexp.MustCompile(unamePrefixReportedTodoRegexp(keyword))
+	groups := reportedTodo.FindStringSubmatch(line)
+
+	if groups == nil {
+		return nil
+	}
+
+	prefix := groups[1]
+	urgency := groups[2]
+	username := groups[3]
+	id := groups[4]
+	suffix := groups[5]
+	title := project.Title.Transform(suffix)
+
+	return &Todo{
+		Prefix:        prefix,
+		Suffix:        suffix,
+		Keyword:       keyword,
+		Urgency:       len(urgency),
+		Username:      username,
+		ID:            &id,
+		Filename:      "",
+		Line:          0,
+		Title:         title,
+		BodySeparator: project.BodySeparator,
+	}
 }
 
 // LineAsTodo constructs a Todo from a string
 func (project Project) LineAsTodo(line string) *Todo {
-	if todo := project.lineAsUnreportedTodo(line); todo != nil {
-		return todo
-	}
+	for _, keyword := range project.Keywords {
+		var todo *Todo
 
-	if todo := project.lineAsReportedTodo(line); todo != nil {
-		return todo
+		if project.UseUsernames {
+			// Try parse an username-prefixed, reported todo
+			// This is more specific so it goes first
+			if todo := project.lineAsUsernamePrefixReportedTodo(line, keyword); todo != nil {
+				return todo
+			}
+
+			// Try parse an username-prefixed, unreported todo
+			if todo := project.lineAsUsernamePrefixUnreportedTodo(line, keyword); todo != nil {
+				return todo
+			}
+
+			noUsernamePrefix := false
+			originalID := ""
+
+			if nonPrefixedTodo := project.lineAsSimpleReportedTodo(line, keyword); nonPrefixedTodo != nil {
+				noUsernamePrefix = true
+				if nonPrefixedTodo.ID != nil {
+					originalID = *nonPrefixedTodo.ID
+				}
+			} else if nonPrefixedTodo := project.lineAsSimpleUnreportedTodo(line, keyword); nonPrefixedTodo != nil {
+				noUsernamePrefix = true
+				todo = nonPrefixedTodo
+			}
+
+			if noUsernamePrefix && todo != nil {
+				// Set a username for updating existing todos
+				todo.Username = getGitUserName()
+				todo.ID = &originalID
+				return todo
+			}
+		} else {
+			if todo := project.lineAsSimpleReportedTodo(line, keyword); todo != nil {
+				return todo
+			}
+
+			if todo := project.lineAsSimpleUnreportedTodo(line, keyword); todo != nil {
+				return todo
+			}
+		}
 	}
 
 	return nil
@@ -212,7 +347,7 @@ func (project Project) WalkTodosOfDir(dirpath string, visit func(Todo) error) er
 		}
 
 		if stat.IsDir() {
-			// FIXME(#145): snitch should go inside of git submodules recursively
+			// FIXME(#145)[#6]: snitch should go inside of git submodules recursively
 			fmt.Printf("[WARN] `%s` is probably a submodule. Skipping it for now...\n", filepath)
 			continue
 		}
@@ -246,6 +381,7 @@ func NewProject(filePath string) (*Project, error) {
 		},
 		Keywords:      []string{},
 		BodySeparator: defaultBodySeparator,
+		UseUsernames:  false,
 	}
 
 	if configPath, ok := yamlConfigPath(filePath); ok {
